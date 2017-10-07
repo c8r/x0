346b2b30e0cc4e9c6324212fbb95feba95135e91
Brent Jackson <jxnblk@gmail.com>
2017-10-07 09:38:46 -0400

    Experimenting with Head component

--- docs/App.js
+++ docs/App.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import { createProvider } from 'refunk'
 import Title from './Title'
+// import { Head } from '../lib'
 
 const dec = state => ({ count: state.count - 1 })
 const inc = state => ({ count: state.count + 1 })
@@ -9,6 +10,9 @@ const Debug = props => <pre children={JSON.stringify(props, null, 2)} />
 
 const App = props => (
   <div>
+    <title>Hello x0</title>
+    <meta charSet='utf-8' />
+    <style>{`body{background-color:gray}`}</style>
     <Title>
       Hello {props.count}
     </Title>
