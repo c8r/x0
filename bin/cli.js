ec77fcc31b8454e7653d5329f18d540fdfb43c98
John Otander <johnotander@gmail.com>
2017-10-16 10:18:21 -0600

    Let's not alias this option

--- bin/cli.js
+++ bin/cli.js
@@ -27,7 +27,7 @@ const cli = meow(`
 
     -o --open     Open dev server in default browser
 
-    -x --proxy   Proxy requests to another server (only for dev)
+    --proxy       Proxy requests to another server (only for dev)
 
 `, {
   alias: {
