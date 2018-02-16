82564a02ba5a3d8c0e7057e93a24f84fd1cea093
Brent Jackson <jxnblk@gmail.com>
2018-02-16 09:29:00 -0500

    Typo

--- examples/webpack-config/App.js
+++ examples/webpack-config/App.js
@@ -3,7 +3,7 @@ import readme from './README.md'
 
 const App = props => (
   <div>
-    <h1>Hello markdownl-loader</h1>
+    <h1>Hello markdown-loader</h1>
     <div
       dangerouslySetInnerHTML={{
         __html: readme
