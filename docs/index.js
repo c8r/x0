3751b5923cfd4ad30a4d070d758cc3a67ab14c35
Brent Jackson <jxnblk@gmail.com>
2018-05-20 15:59:08 -0400

    Add JSX support

--- docs/index.js
+++ docs/index.js
@@ -1,9 +1,31 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 export default class extends React.Component {
   render () {
     return (
-      <h1>x0</h1>
+      <React.Fragment>
+        <h1>x0</h1>
+        <ul>
+          <li>
+            <Link to='/'>Home</Link>
+          </li>
+          <li>
+            <Link to='/hello'>Hello JSX</Link>
+          </li>
+        </ul>
+        <video
+          style={{
+            maxWidth: '100%',
+            height: 'auto'
+          }}
+          autoPlay
+          loop
+          muted
+          playsInline>
+          <source src='/demo.mp4' />
+        </video>
+      </React.Fragment>
     )
   }
 }
