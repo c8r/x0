5d5a3ecd4c65ee8faf804952ffc883da34f7df4e
Brent Jackson <jxnblk@gmail.com>
2018-05-25 12:14:43 -0400

    Update styled-components example

--- examples/styled-components/pages/index.js
+++ examples/styled-components/pages/index.js
@@ -1,9 +1,6 @@
 import React from 'react'
-import styled, { ServerStyleSheet } from 'styled-components'
+import styled from 'styled-components'
 import { connect } from 'refunk'
-
-const css = `*{box-sizing:border-box}
-body{font-family:-apple-system,BlinkMacSystemFont,sans-serif;margin:0}`
 
 const Root = styled('div')([], {
   padding: '48px'
@@ -44,37 +41,19 @@ const dec = state => ({ count: state.count - 1 })
 const inc = state => ({ count: state.count + 1 })
 
 const App = connect(props => [
-  props.styles && (
-    <head
-      dangerouslySetInnerHTML={{
-        __html: props.styles || ''
-      }}
-    />
-  ),
   <Root>
-    <div id='app'>
-      <title>x0 styled-components {props.count}</title>
-      <style dangerouslySetInnerHTML={{ __html: css }} />
-      <Heading>Hello x0 styled-components</Heading>
-      <Button
-        color={colors[props.count % colors.length]}
-        onClick={e => props.update(inc)}>
-        Beep {props.count}
-      </Button>
-    </div>
+    <Heading>Hello x0 styled-components</Heading>
+    <Button
+      color={colors[props.count % colors.length]}
+      onClick={e => props.update(inc)}>
+      Beep {props.count}
+    </Button>
   </Root>
 ])
 
 App.defaultProps = {
   styles: false,
   count: 0
-}
-
-App.getInitialProps = ({ Component, props }) => {
-  const sheet = new ServerStyleSheet()
-  sheet.collectStyles(<Component {...props} />)
-  const styles = sheet.getStyleTags()
-  return { styles }
 }
 
 export default App
